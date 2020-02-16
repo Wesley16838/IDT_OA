e303d5f8c16e287d65e459a3a47acc5bce613de0
翁尉瑄 <wesley@ouyasushis-MacBook-Pro.local>
2020-02-15 19:37:16 -0500

    run build

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/history', async (req , res) => {
 });
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname+'/client/build/index.html'))
+    res.sendFile(path.resolve(__dirname+'/client/public/build/index.html'))
 })
 
 const PORT = process.env.PORT || 5000;
